8d4f28e4d7ae99278a808f66b808156a8f34630c
Leon Frajmund <leonfrajmund@gmai.com>
2014-05-21 10:07:51 -0400

    yo have player stuff

---
+++ Player.java
@@ -0,0 +1,28 @@
+public abstract class Player{
+    protected String name, phrase;
+    protected double health, damage, defense, caffeine;
+    protected int XP, level;
+    protected skillTree skills;
+    
+
+    public double getHealth(){return health;}
+    public double getDamage(){return damage;}
+    public double getDefense(){return defense;}
+    public double getCaffeine(){return caffeine;}
+    public int getXP(){return XP;}
+    public int getLevel(){return level;}
+    public String getName(){return name;}
+    public String talk(){return phrase;}
+
+    public void setName(String n){name = n;}
+    public void setHealth(double n){health = n;}
+    public void setXP(int n){XP = n;}
+    public void setPhrase(String n){phrase = n;}
+    public void setDamage(double n){damage = n;}
+    public void setDefense(double n){defense = n;}
+    public void setLevel(int n){level = n;}
+    public void setCaffeine(double n){caffeine = n;}
+
+    public abstract void skill(int n);
+
+}

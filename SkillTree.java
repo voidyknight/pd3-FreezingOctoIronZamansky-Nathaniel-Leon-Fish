65945fe5c4a7804cc11774369d4bc14b32c09e85
Fish Milnikiewicz <fmilniki@gmail.com>
2014-05-21 10:11:32 -0400

    SkillTree.java

---
+++ SkillTree.java
@@ -0,0 +1,10 @@
+public class SkillTree(){
+
+    public String[] SkillTree;
+
+    public SkillTree(){
+	SkillTree = {"Anonymous","Web_Developer","Hacker","Software","Digital","Game_Developer","Win_The_Game","CS","Biological Computer","Computers","HAL","Hardware","Cyberman","Robotics","Cyborg"}
+    }
+
+
+}

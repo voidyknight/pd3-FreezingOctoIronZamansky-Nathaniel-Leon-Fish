f370a5a84fd11098f89c17b30e4a3615043009b6
Nathaniel Brassell <nathaniel303@gmail.com>
2014-05-21 21:07:41 -0400

    Update Virus.java
    made constructor... some of these vars should be methodes

--- Virus.java
+++ Virus.java
@@ -3,7 +3,8 @@ public class Virus extends Monster{
   public Virus(){
     super.setName("Elk Cloner");
     super.setLevel(1);
-    super.setHealth()
-
+    super.setHealth(100);
+    super.setDamage(5);
+    super.setDefense(5);
     }
 }

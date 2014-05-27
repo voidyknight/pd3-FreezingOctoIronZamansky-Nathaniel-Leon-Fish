add3ae1eb28618f9aa71cfdbc06fe0d82026a836
Leon Frajmund <leonfrajmund@gmai.com>
2014-05-27 09:34:55 -0400

    sdf

--- SkillTree.java
+++ SkillTree.java
@@ -1,10 +1,42 @@
+import java.util.Scanner;
 public class SkillTree(){
-
-    public String[] SkillTree;
+    private Scanner sc = new Scanner(System.in);
+    private final String[] skills = {null, "CS", "Software", "Hardware", 
+				     "Web Developer", "Game Developer",
+				     "Computer", "Robotics", "Anonymous", 
+				     "Hacker", "Digitalize", "Win the Game", 
+				     "Biological Computer", "HAL", "Cyberman",
+				     "Cyborg"};
+    private int[] acquiredSkills;
 
     public SkillTree(){
-	SkillTree = {"Anonymous","Web_Developer","Hacker","Software","Digital","Game_Developer","Win_The_Game","CS","Biological Computer","Computers","HAL","Hardware","Cyberman","Robotics","Cyborg"}
+	acquiredSkills = {1, -1, -1, -1};
     }
+
+    public void getNewSkill(){
+	int last = 3;
+	while(acquiredSkills[last] < 0)
+	    last --;
+	if(last == 3) return;
+	System.out.println("Choose a new Skill: press 1 for " + 
+			   skills[2*acquiredSkills[last]]+" or 2 for" +
+			   skills[2*acquiredSkills[last] + 1]);
+	int response = sc.nextInt();
+	while(response != 1 || response != 2){
+	    System.out.println("Invalid input");
+	    System.out.println("Choose a new Skill: press 1 for " + 
+			       skills[2*acquiredSkills[last]]+" or 2 for" +
+			       skills[2*acquiredSkills[last] + 1]);
+	    int response = sc.nextInt();
+	}
+	if(response == 1)
+	    acquiredSkills[last + 1] = skills[2*acquiredSkills[last]];
+	else
+	    acquiredSkills[last + 1] = skills[2*acquiredSkills[last] + 1]);
+    
+}
+	
+}
 
 
 }

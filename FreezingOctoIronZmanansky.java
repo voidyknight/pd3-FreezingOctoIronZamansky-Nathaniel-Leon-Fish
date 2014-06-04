207ae21de62fe2db96869d7e19cfffc287c8c71d
Nathaniel Brassell <nathaniel303@gmail.com>
2014-06-04 09:59:49 -0400

    Create FreezingOctoIronZmanansky.java

---
+++ FreezingOctoIronZmanansky.java
@@ -0,0 +1,14 @@
+public class FreezingOctoIronZamansky extends Monster{
+
+  public FreezingOctoIronZamansky(){
+    super.setName("1337");
+    super.setLevel(10);
+    super.setHealth(1000);
+    super.setDamage(50);
+    super.setDefense(50);
+    super.setRange(10);
+    }
+    
+  public void summonMinions(){
+  }
+}

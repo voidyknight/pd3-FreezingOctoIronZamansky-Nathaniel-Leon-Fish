d79da76fc24abf29fe4b97b42ea95967c4031906
Nathaniel Brassell <nathaniel303@gmail.com>
2014-06-04 09:50:39 -0400

    Create Rumba.java

---
+++ Rumba.java
@@ -0,0 +1,11 @@
+public class Virus extends Monster{
+
+  public Virus(){
+    super.setName("Elk Cloner");
+    super.setLevel(1);
+    super.setHealth(100);
+    super.setDamage(5);
+    super.setDefense(5);
+    super.setRange(5);
+    }
+}

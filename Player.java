84bd1eb6318afd2863ffa0b0279d06ff147e501c
Leon Frajmund <leonfrajmund@gmai.com>
2014-06-06 10:09:44 -0400

    Player.java: attack function changed

--- Player.java
+++ Player.java
@@ -1,5 +1,6 @@
 public abstract class Player{
     protected String name, phrase;
+    protected Map map;
 
     protected double health, damage, defense, caffeine, swag; //swag = mana
     protected int XP, level, range, xpos, ypos;
@@ -10,7 +11,7 @@ public abstract class Player{
     public double getHealth(){return health;}
     public double getDamage(){return damage;}
     public double getDefense(){return defense;}
-    public double getCaffeine(){return caffeine;}
+    public double getSwag(){return swag;}
     public int getXP(){return XP;}
     public int getLevel(){return level;}
     public int getRange(){return range;}
@@ -26,12 +27,32 @@ public abstract class Player{
     public void setDamage(double n){damage = n;}
     public void setDefense(double n){defense = n;}
     public void setLevel(int n){level = n;}
-    public void setCaffeine(double n){caffeine = n;}
+    public void setSwag(double n){swag = n;}
     public void setRange(int n ){range = n;}
     public void setXpos(int n){xpos = n;}
     public void setYpos(int n){ypos = n;}
 
-    public double attack(Monster target){return ((damage * Math.random()) - target.getDefense());}
+    public void attack(){
+	Monster target = null;
+	for(int x = -1; x < 2; x ++){
+	    for(int y = -1; y < 2; y ++){
+		if(!Map.get(x,y).equals(" ") && !Map.get(x,y).equals("#") && !Map.get(x,y).equals("p")){
+		    target = Map.getMonster(x,y);
+		    x += 10;
+		    y += 10;
+		}
+	    }
+	}
+	attack(target);
+    }
+
+
+    public void attack(Monster target){
+	if(target == null)
+	    return;
+	target.setHealth(target.getHealth() - 
+			 ((damage * Math.random()) - target.getDefense()));
+    }
 
     public abstract void skill(int n);
 

8bb1ba215e34ad69c139cd605d8064fbe2145113
Nathaniel Brassell <nathaniel303@gmail.com>
2014-06-09 10:14:17 -0400

    Update Brooks.java

--- Brooks.java
+++ Brooks.java
@@ -1,6 +1,15 @@
 public class Brooks extends Player{
 
   public Brooks(){
+    setSwag(100.0);
+    setHealth(150.0);
+    setSwagRegenSpeed(4);
+    setHealthRegenSpeed(6);
+    setDamage(100/6);
+    setDefense(1.1001);
+    setRange(1);
+    setName("Mistear Schwager");
+    setPhrase("Wassssssssssup");
   }
 
 

4784f00b158f99c266e0f6eb4286ba58232a68b2
Nathaniel Brassell <nathaniel303@gmail.com>
2014-06-09 10:09:50 -0400

    Update Virus.java

--- Virus.java
+++ Virus.java
@@ -1,25 +1,25 @@
 public class Virus extends Monster{
 
     public Virus(){
-	super.setName("Elk Cloner");
-	super.setLevel(1);
-	super.setHealth(100);
-	super.setDamage(5);
-	super.setDefense(5);
-	super.setRange(1);
+	setName("Elk Cloner");
+	setLevel(1);
+	setHealth(100);
+	setDamage(5);
+	setDefense(5);
+	setRange(1);
     }
 
     public Virus(int i, int x, int y){//used in Map class
-	super.setName("Elk Cloner");
-	super.setLevel(1);
-	super.setHealth(100);
-	super.setDamage(5);
-	super.setDefense(5);
-	super.setRange(1);
+	setName("Elk Cloner");
+	setLevel(1);
+	setHealth(100);
+	setDamage(5);
+	setDefense(5);
+	setRange(1);
 
-	super.setID(i);
-	super.setXpos(x);
-	super.setYpos(y);
+	setID(i);
+	setXpos(x);
+	setYpos(y);
     }
 
     public void skill(int n){}

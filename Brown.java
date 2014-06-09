8e19c5067ea8dabf3d86d743dfe8848076df08db
Nathaniel Brassell <nathaniel303@gmail.com>
2014-06-09 10:13:19 -0400

    Update Brown.java

--- Brown.java
+++ Brown.java
@@ -1,11 +1,10 @@
 public class Brown extends Player{
 
   public Brown(){
-    setSwag(150);
-    setHealth(100);
+    setSwag(150.0);
+    setHealth(100.0);
     setSwagRegenSpeed(6);
     setHealthRegenSpeed(4);
-    setHealth(100.0);
     setDamage(100/6);
     setDefense(1.100000000000000000000001);
     setRange(1);

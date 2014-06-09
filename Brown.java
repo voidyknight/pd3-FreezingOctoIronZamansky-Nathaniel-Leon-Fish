95d7b9605e5f28ec17f56cca618757ff000b4508
Nathaniel Brassell <nathaniel303@gmail.com>
2014-06-09 10:11:09 -0400

    Update Brown.java

--- Brown.java
+++ Brown.java
@@ -1,12 +1,15 @@
 public class Brown extends Player{
 
-private int infinite = 999999999999999999999999999999999999999999999999999999999999999;
-
   public Brown(){
-    swag = infinite;
-    phrase = "Wassssssssssup";
+    setSwag(150);
+    setHealth(100);
+    setSwagRegenSpeed(6);
+    setHealthRegenSpeed(4);
+    setHealth(100.0);
+    setDamage(100/6);
+    setDefense(1.100000000000000000000001);
+    setRange(1);
+    setName("Mistear Schwager");
+    setPhrase("Wassssssssssup");
   }
-
-
-
 }

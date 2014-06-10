5e57d3a1b2e2b6abd05109864f5ca87b7ce60ad2
Nathaniel Brassell <nathaniel303@gmail.com>
2014-06-09 22:05:39 -0400

    Update Zamansky.java

--- Zamansky.java
+++ Zamansky.java
@@ -1,8 +1,14 @@
 public class Zamansky extends Player{
 
   public Zamansky(){
+    setSwag(100.0);
+    setHealth(100.0);
+    setSwagRegenSpeed(4);
+    setHealthRegenSpeed(4);
+    setDamage(100/6);
+    setDefense(1.1);
+    setRange(3);
+    setName("");
+    setPhrase("");
   }
-
-
-
 }

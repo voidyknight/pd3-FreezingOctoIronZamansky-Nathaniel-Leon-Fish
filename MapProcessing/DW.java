1705341ac85371ece66bc14a5ee964ac0fc191dd
Nathaniel Brassell <nathaniel303@gmail.com>
2014-06-10 10:00:46 -0400

    Create DW.java

---
+++ MapProcessing/DW.java
@@ -0,0 +1,14 @@
+public class DW extends Player{
+
+public DW(){
+    setSwag(125.0);
+    setHealth(125.0);
+    setSwagRegenSpeed(5);
+    setHealthRegenSpeed(5);
+    setDamage(100/5);
+    setDefense(2.2);
+    setRange(2);
+    setName("");
+    setPhrase("");
+    }
+}
